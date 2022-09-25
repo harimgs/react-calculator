32860972ef603c64ac1c0289ceb3ee0d6610f93c
harimgs <harimgs@hotmail.com>
2022-09-25 16:25:43 -0400

    basic ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calculator</h1>
+      <div className="container">
+        <input></input>
+        <table>
+          <thead></thead>
+          <tbody>
+            <tr>
+              <td>
+                <Button variant="contained">C</Button>
+              </td>
+              <td>
+                <Button variant="contained">()</Button>
+              </td>
+              <td>
+                <Button variant="contained">%</Button>
+              </td>
+              <td>
+                <Button variant="contained">÷</Button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Button variant="contained">7</Button>
+              </td>
+              <td>
+                <Button variant="contained">8</Button>
+              </td>
+              <td>
+                <Button variant="contained">9</Button>
+              </td>
+              <td>
+                <Button variant="contained">X</Button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Button variant="contained">4</Button>
+              </td>
+              <td>
+                <Button variant="contained">5</Button>
+              </td>
+              <td>
+                <Button variant="contained">6</Button>
+              </td>
+              <td>
+                <Button variant="contained">-</Button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Button variant="contained">1</Button>
+              </td>
+              <td>
+                <Button variant="contained">2</Button>
+              </td>
+              <td>
+                <Button variant="contained">3</Button>
+              </td>
+              <td>
+                <Button variant="contained">+</Button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <Button variant="contained">+/-</Button>
+              </td>
+              <td>
+                <Button variant="contained">0</Button>
+              </td>
+              <td>
+                <Button variant="contained">.</Button>
+              </td>
+              <td>
+                <Button variant="contained">=</Button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
